fef32508f404a4be7ef8101c62dbcb529c06c232
mohid <mohid10587@gmail.com>
2023-05-20 20:24:52 +0600

    ok

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div className='flex justify-center items-center min-h-screen bg-cover' style={{ backgroundImage: 'url("/bg.jpg")', backgroundRepeat:'no-repeat', }}>
+      <h1 className='text-3xl text-white px-6'>Our awesome business management system is coming soon</h1>
+    </div>
+  );
+}
+
+export default App;
