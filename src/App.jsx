0040a4b4eddd6cee781a3c3655c6f3119e4b7857
mohid <mohid10587@gmail.com>
2023-05-20 20:29:34 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className='flex justify-center min-h-screen bg-cover' style={{ backgroundImage: 'url("/bg.jpg")', backgroundRepeat:'no-repeat', }}>
      <div className="mt-24">
+      
      <h1 className='text-2xl text-white px-6 text-center'>Our awesome business management system is coming soon</h1>
       <h1 className='text-2xl  px-6 text-center text-orange-600'>Md. Feroz Ahamed</h1>
      </div>
