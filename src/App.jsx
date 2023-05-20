ec55655758db73d5a5259b4bfcdcf8bab422cd05
mohid <mohid10587@gmail.com>
2023-05-20 20:41:43 +0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@
 
 function App() {
   return (
-    <div className='flex justify-center min-h-screen bg-cover' style={{ backgroundImage: 'url("/bg.jpg")', backgroundRepeat:'no-repeat', }}>
+    <div className='flex justify-center min-h-screen ' style={{ backgroundImage: 'url("/bg.jpg")', backgroundRepeat:'no-repeat', }}>
      <div className="mt-24">
-      
-     <h1 className='text-2xl text-white px-6 text-center'>Our awesome business management system is coming soon</h1>
-      <h1 className='text-2xl  px-6 text-center text-orange-600'>Md. Feroz Ahamed</h1>
+     <h1 className='text-2xl text-white px-6 text-center'>Our awesome business management system is coming soon !!</h1>
+      <h1 className='text-2xl  px-6 text-center text-orange-600 mt-44'>Md. Feroz Ahamed</h1>
      </div>
 
     </div>
